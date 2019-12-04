47e6a072c389d174cb1fe07832ec369d5825e9cd
Santiago Manganaro Bello <smanganaro@itba.edu.ar>
2019-12-04 17:16:38 -0300

    Core game finished

---
+++ SuperHexa/Assets/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+   	public float moveSpeed = 400f;
+
+   	float movement = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+       movement = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+    	transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+    	SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); 
+    }
+
+
+}
